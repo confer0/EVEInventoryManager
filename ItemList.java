c810095c1a92a2a39f4b0ab1dd676dc910e9e31f
micahbakan <bakan.micah@gmail.com>
2017-05-23 14:27:02 -0400

    Update ItemList.java

--- ItemList.java
+++ ItemList.java
@@ -117,10 +117,87 @@ public class ItemList{
     public double getVolume(String name){
         return getItem(name).getVolume();
     }
+    public void AlphaSort() {
+		int n = items.size();
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j < (n - i); j++) {
+				if ((items.get(j - 1).getName()).compareToIgnoreCase(items.get(j).getName()) > 0) {
+					items.set(j - 1, items.get(j));
+					items.remove(j + 1);
+				}
+
+			}
+		}
+	}
+	public void ReverseAlphaSort() {
+		int n = items.size();
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j < (n - i); j++) {
+				if ((items.get(j - 1).getName()).compareToIgnoreCase(items.get(j).getName()) < 0) {
+					items.set(j - 1, items.get(j));
+					items.remove(j + 1);
+				}
+
+			}
+		}
+	}
+	public void AmtSortGtoS() {
+		//greatest to smallest
+		int n = items.size();
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j < (n - i); j++) {
+				if (items.get(j - 1).getQnt()<items.get(j).getQnt()) {
+					items.set(j - 1, items.get(j));
+					items.remove(j + 1);
+				}
+
+			}
+		}
+	}
+	public void AmtSortStoG() {
+		//greatest to smallest
+		int n = items.size();
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j < (n - i); j++) {
+				if (items.get(j - 1).getQnt()>items.get(j).getQnt()) {
+					items.set(j - 1, items.get(j));
+					items.remove(j + 1);
+				}
+
+			}
+		}
+	}
+	public void ValSortStoG() {
+		//greatest to smallest
+		int n = items.size();
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j < (n - i); j++) {
+				if (items.get(j - 1).getTotalPrice()>items.get(j).getTotalPrice()) {
+					items.set(j - 1, items.get(j));
+					items.remove(j + 1);
+				}
+
+			}
+		}
+	}
+	public void ValSortGtoS() {
+		//greatest to smallest
+		int n = items.size();
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j < (n - i); j++) {
+				if (items.get(j - 1).getTotalPrice()<items.get(j).getTotalPrice()) {
+					items.set(j - 1, items.get(j));
+					items.remove(j + 1);
+				}
+
+			}
+		}
+	}
 
     //returns err somting
     public String toString(){
         return "yo";
     }
+    
 
 }
